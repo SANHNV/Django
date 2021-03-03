5a7bc12488426bf6c31131a8ce4472e780328491
manfredhq <hugo.quitton@gmail.com>
2021-03-03 15:09:38 +0100

    main as component

--- src/main/java/com/django/Main.java
+++ src/main/java/com/django/Main.java
@@ -2,13 +2,17 @@ package com.django;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Main {
+
 
 	public static void main(String[] args) {
 		ApplicationContext myApplicationContext = new AnnotationConfigApplicationContext("com.django");
 		System.out.println("Hello World!");
-		System.out.println(myApplicationContext);
+		System.out.println(myApplicationContext.getApplicationName());
+		System.out.println(myApplicationContext.getBean(Main.class));
 		return;
 	}
 
