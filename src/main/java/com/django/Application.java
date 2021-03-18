129bb74f32bb889913ef9af8a1e999466f06c6b1
SANHNV <45290025+SANHNV@users.noreply.github.com>
2021-03-18 16:41:24 +0100

    port config

--- src/main/java/com/django/Application.java
+++ src/main/java/com/django/Application.java
@@ -8,24 +8,14 @@ import com.django.test.Test;
 import org.hibernate.HibernateException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args){
 		SpringApplication.run(Application.class, args);
-		main2(args);
-	}
-
-	public static void main2(String[] args){
-
 
 		System.out.println("Hello World!");
 
