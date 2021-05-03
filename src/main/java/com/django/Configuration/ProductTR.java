3cafd78b61c830f55bfdd03588d22e42590f1e4b
Hugo <hugo.quitton@gmail.com>
2021-05-03 22:04:35 +0200

    product view
    Request to get all products.
    Show all products in the table on the page productList

--- src/main/java/com/django/Configuration/ProductTR.java
+++ src/main/java/com/django/Configuration/ProductTR.java
@@ -1,7 +1,11 @@
 package com.django.Configuration;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.django.Models.Product;
+import com.mysql.cj.Query;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -18,6 +22,10 @@ public class ProductTR {
 
     public Product getProductById(Integer id) {
 		return sessionFactory.getCurrentSession().createQuery("select d from Product d where d.id = :id", Product.class).setParameter("id", id).getSingleResult();
+	}
+
+	public List<Product> getProducts(){
+		return sessionFactory.getCurrentSession().createNativeQuery("select * from Product", Product.class).getResultList();
 	}
 
 	public void saveUser(Product product) {
--- src/main/java/com/django/Controllers/ProductListController.java
+++ src/main/java/com/django/Controllers/ProductListController.java
@@ -3,6 +3,15 @@ package com.django.Controllers;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.django.Configuration.ProductTR;
+import com.django.Models.Product;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 //import org.springframework.web.servlet.ModelAndView;
 import org.springframework.stereotype.Controller;
 
@@ -12,7 +21,12 @@ public class ProductListController {
     @RequestMapping({"/productList"})
     public String showProducts(@RequestParam(value = "name", defaultValue = "World", required = true) String name, Model model) {
         System.out.println("controller products");
+        AnnotationConfigApplicationContext  myApplicationContext = new AnnotationConfigApplicationContext("com.django");
         model.addAttribute("name", name);
+        List<Product> products = myApplicationContext.getBean(ProductTR.class).getProducts();
+        model.addAttribute("productList",products);
+        System.out.println(products.size());
+        myApplicationContext.close();
         return "productListView";
     }
 
