f6fb9c985a865b27db986d8ea723a501ddd809c4
SANHNV <45290025+SANHNV@users.noreply.github.com>
2021-05-03 21:36:55 +0200

    add product transaction file

---
+++ src/main/java/com/django/Configuration/ProductTR.java
@@ -0,0 +1,27 @@
+package com.django.Configuration;
+
+
+import com.django.Models.Product;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Transactional
+public class ProductTR {
+
+    @Autowired
+	private SessionFactory sessionFactory;
+
+    public Product getProductById(Integer id) {
+		return sessionFactory.getCurrentSession().createQuery("select d from Product d where d.id = :id", Product.class).setParameter("id", id).getSingleResult();
+	}
+
+	public void saveUser(Product product) {
+		Session session = sessionFactory.getCurrentSession();
+        session.save(product);
+	}
+}
